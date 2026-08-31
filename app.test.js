ddd673cc5d8dca3c10cba6736c13be570a9c2a40
Akshayjain7208 <146410317+Akshayjain7208@users.noreply.github.com>
2026-08-31 22:32:23 +0530

    Replace expect with assert in test case

--- app.test.js
+++ app.test.js
@@ -1,5 +1,7 @@
+const test = require('node:test');
+const assert = require('node:assert');
 const add = require('./app');
 
 test('adds two numbers', () => {
-    expect(add(2, 3)).toBe(5);
+    assert.strictEqual(add(2, 3), 5);
 });
